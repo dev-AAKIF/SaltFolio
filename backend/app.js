78c7919dee7240038c01ab8124d86fd6fb13158a
Akif <akif7868@gmail.com>
2025-08-08 23:17:51 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use('/api/v1/portfolio',portfolioRoutes)
 app.use(
   cors({
     // origin:process.env.ORIGIN    ,
-    origin: "https://selfolio-client.onrender.com",
+    origin: "https://saltfolio-frontend.onrender.com",
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"], 
 
